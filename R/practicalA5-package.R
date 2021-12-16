fff326157e2fcc93444ba98f73b8ee6b3535faf0
Richard Reeve <git@richardreeve.net>
2021-12-16 15:22:07 +0000

    Package basics

---
+++ R/practicalA5-package.R
@@ -0,0 +1,15 @@
+#' @title Example code for practical A-5
+#'
+#' @description
+#' \code{practicalA5} is a simple package that demonstrates writing nested
+#' functions - so functions that provide simpler versions of other functions
+#'
+#' @author
+#' Richard Reeve <richard.reeve@glasgow.ac.uk>
+
+#' @name practicalA5-package
+#' @aliases practicalA5
+#' @docType package
+#'
+#'
+NULL
